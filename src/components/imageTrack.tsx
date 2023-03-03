7bbf7bf4fedd3369a78fe6968f76a8542b11ce6c
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-03-03 09:10:25 -0300

    adicionando handlers

--- src/components/imageTrack.tsx
+++ src/components/imageTrack.tsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactCSSTransitionGroup from 'react-transition-group'; 
 
 type Props = {
   children: JSX.Element[];
 };
 
 const Index = ({ children }: Props) => {
+  const [mouseDownAt, setMouseDownAt] = useState(0);
+  const [percentage, setPercentage] = useState(0);
+  const [prevPercentage, setPrevPercentage] = useState(0);
+
+  function handleMouseDown(ev: React.MouseEvent) {
+    setMouseDownAt(ev.clientX);
+  }
+
+  function handleMouseUp(ev: React.MouseEvent) {
+    setMouseDownAt(0);
+    setPrevPercentage(percentage);
+  }
+
+  function handleMouseMove(ev: React.MouseEvent) {
+    if (mouseDownAt === 0) return;
+
+    const mouseDelta = mouseDownAt - ev.clientX;
+    const maxDelta = window.innerWidth / 2;
+
+    const _percentage = (mouseDelta / maxDelta) * -100;
+    const _nextPercentage = Math.max(Math.min((prevPercentage + _percentage), 0), -100)
+
+    setPercentage(_nextPercentage);
+  }
+
   return (
-    <div id="image-track" data-mouse-down-at="0">
+    <div
+      data-mouse-down-at={mouseDownAt}
+      onMouseDown={(ev) => handleMouseDown(ev)}
+      onMouseUp={(ev) => handleMouseUp(ev)}
+      onMouseMove={(ev) => handleMouseMove(ev)}
+    >
       {children}
     </div>
   );
